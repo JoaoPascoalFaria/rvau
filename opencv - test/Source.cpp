b527419c7610fe1e290659ca72ddd50353ed8eee
JoaoPascoalFaria <ei11103@fe.up.pt>
2015-12-14 14:10:48 +0000

    best rectify

--- opencv - test/Source.cpp
+++ opencv - test/Source.cpp
@@ -7,6 +7,10 @@
 
 using namespace cv;
 
+const unsigned NUM_CARDS = 56;
+const float CARD_SIZE_X = 250;
+const float CARD_SIZE_Y = 350;
+
 bool compareContourAreas ( std::vector<cv::Point> contour1, std::vector<cv::Point> contour2 ) {
     double i = fabs( contourArea(cv::Mat(contour1)) );
     double j = fabs( contourArea(cv::Mat(contour2)) );
@@ -30,18 +34,65 @@ void rectify(Point2f points[4]) {
 		if (diff[i]<smallest_diff){smallest_diff=diff[i];smallest_diff_p=points[i];}
 		if (diff[i]>biggest_diff){biggest_diff=diff[i];biggest_diff_p=points[i];}
 	}
-	for(unsigned i=0;i<4;i++)std::cout<<"points["<<i<<"] = "<<points[i]<<" sum:"<<sum[i]<<" diff:"<<diff[i]<<std::endl;
+	//for(unsigned i=0;i<4;i++)std::cout<<"points["<<i<<"] = "<<points[i]<<" sum:"<<sum[i]<<" diff:"<<diff[i]<<std::endl;
 	points[0] = smallest_sum_p;
-	points[1] = smallest_diff_p;
+	points[3] = smallest_diff_p;
 	points[2] = biggest_sum_p;
-	points[3] = biggest_diff_p;
+	points[1] = biggest_diff_p;
+	//for(unsigned i=0;i<4;i++)std::cout<<"points["<<i<<"] = "<<points[i]<<std::endl;
+}
+
+void rectify2(Point2f points[4]) {
+	Point2f pA;
+	float best_dis=-1,this_dis;
+	unsigned index=0;
+	//distancias p->p2
+	for(unsigned i=0;i<3;i++){
+		this_dis = norm(points[i]-points[i+1]);
+		if(this_dis>best_dis){
+			best_dis = this_dis;
+			index=i;
+			pA = points[i];
+		}
+	}
 	for(unsigned i=0;i<4;i++)std::cout<<"points["<<i<<"] = "<<points[i]<<std::endl;
+	if(index==0) {
+		points[0] = points[1];
+		points[1] = points[2];
+		points[2] = points[3];
+		points[3] = pA;
+	}
+	else if(index==2) {
+		points[0] = points[1];
+		points[1] = points[2];
+		points[2] = points[3];
+		points[3] = pA;
+	}
+	for(unsigned i=0;i<4;i++)std::cout<<"points["<<i<<"] = "<<points[i]<<std::endl;
+}
+
+void rectify3(Point2f points[4]) {
+	//fix horizontal card
+	if(norm(points[0]-points[1]) > norm(points[1]-points[2])) {
+		Point2f firstP = points[0];
+		points[0] = points[1];
+		points[1] = points[2];
+		points[2] = points[3];
+		points[3] = firstP;
+	}
+	//mirror Y axis
+	Point2f temp = points[0];
+	points[0] = points[1];
+	points[1] = temp;
+	temp = points[2];
+	points[2] = points[3];
+	points[3] = temp;
 }
 
 void toPoints( std::vector<Point> approx, Point2f rect_points[4] ) {
 	for(unsigned i=0; i<4; i++)
 		rect_points[i] = Point2f(approx[i]);
-	rectify(rect_points);
+	rectify3(rect_points);
 }
 
 void main() {
@@ -51,7 +102,7 @@ void main() {
 	std::vector<Vec4i> hierarchy;
 
 	// load image
-	img = imread("hearts_play.png");
+	img = imread("cards_deck_fixed.png");
 
 	// grayscale
 	cvtColor(img,gray,COLOR_BGR2GRAY);
@@ -60,19 +111,8 @@ void main() {
 	GaussianBlur(gray,blur,Size(1,1),0,0,1000);
 
 	//threshold
-	threshold(gray,thresh,120,255,THRESH_BINARY);
+	threshold(blur,thresh,120,255,THRESH_BINARY);
 	threshold(blur,flag,120,255,THRESH_BINARY);
-
-	/**//*different approach*/
-	/**/Mat canny;
-	/**/std::vector<Vec4i> lines;
-	/**/
-	/**/// canny
-	/**/Canny( gray, canny, 125, 125);
-	/**/
-	/**///lines around cards
-	/**/HoughLinesP( canny, lines, 1, CV_PI/180, 70, 30 ,10);
-	/**//*end of it*/
 
 
 	//contours
@@ -85,11 +125,11 @@ void main() {
 	double peri;
 	RotatedRect rect;
 	Mat transform, warp;
-	Point2f h[4] = {Point2f(0,0),Point2f(449,0),Point2f(449,449),Point2f(0,449)};
+	Point2f h[4] = {Point2f(0,0),Point2f(CARD_SIZE_X-1,0),Point2f(CARD_SIZE_X-1,CARD_SIZE_Y-1),Point2f(0,CARD_SIZE_Y-1)};
 	Point2f rect_points[4];
-	Mat warps[4];
+	Mat warps[NUM_CARDS];
 
-	for(unsigned i=0; i<4; i++) {
+	for(unsigned i=0; i<NUM_CARDS; i++) {
 	  card = contours[i];
       peri = arcLength(card, true);
 	  approxPolyDP(card, approx, 0.02*peri, true);//std::cout<<"aprox:\n"<<approx<<std::endl;
@@ -98,7 +138,7 @@ void main() {
 	  toPoints( approx, rect_points );//std::cout<<"rect_points:\n";for(unsigned i=0; i<4; i++) std::cout<<rect_points[i]<<std::endl;
 	  transform = getPerspectiveTransform(rect_points,h);
 	  transform = transform*-1;
-	  warpPerspective( img, warp, transform, Size(450,450));
+	  warpPerspective( img, warp, transform, Size(CARD_SIZE_X,CARD_SIZE_Y));
 	  warps[i]=warp.clone();
 	}
 
@@ -130,7 +170,7 @@ void main() {
 		// contour
 		drawContours( drawing, contours, (int)i, color, 1, 8, std::vector<Vec4i>(), 0, Point() );
 	}
-	for( size_t i = 0; i<4; i++ ){
+	for( size_t i = 0; i<NUM_CARDS; i++ ){
 		imshow("warp"+i,warps[i]);
 	}
 	imshow( "OpenCV", drawing );
